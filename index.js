3f03860e17544624585f7b344366f02e0c493fc0
IntPtr <lxy.lixiaoyan@gmail.com>
2016-12-30 00:28:54 +0800

    update config

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-module.exports = {
+module.exports = (context, { modules = false } = {}) => ({
   presets: [
-    require.resolve("babel-preset-latest"),
+    [require.resolve("babel-preset-latest"), {
+      es2015: { modules },
+    }],
     require.resolve("babel-preset-stage-0"),
+    require.resolve("babel-preset-react"),
   ],
   plugins: [
-    require.resolve("babel-plugin-transform-runtime"),
+    [require.resolve("babel-plugin-transform-runtime"), {
+      polyfill: false,
+    }],
+    require.resolve("babel-plugin-transform-decorators-legacy"),
   ],
-};
+});
